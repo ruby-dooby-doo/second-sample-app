4b4961ace759d75f9d6a477ce31df56e4121d8b3
Brian <riceb53@gmail.com>
2018-06-24 10:49:08 -0500

    second route

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -3,4 +3,9 @@ class Api::PagesController < ApplicationController
     @message = "good morning"
     render "good_morning_view.json.jbuilder"
   end
+
+  def tacos_action
+    @message = "tacos are no better than burritos."
+    render "tacos_view.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   get "/good_morning_url" => "api/pages#good_morning_action"
+  get "/tacos_url" => "api/pages#tacos_action"
+
 end
